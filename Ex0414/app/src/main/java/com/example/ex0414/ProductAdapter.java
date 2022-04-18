59db08d160f4033e07c571936d1d24eebae2454f
HeonchanKim <khc9274@naver.com>
2022-04-18 17:03:38 +0900

    0418

--- Ex0414/app/src/main/java/com/example/ex0414/ProductAdapter.java
+++ Ex0414/app/src/main/java/com/example/ex0414/ProductAdapter.java
@@ -1,12 +1,15 @@
 package com.example.ex0414;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 import java.util.ArrayList;
 
@@ -58,7 +61,30 @@ public class ProductAdapter extends BaseAdapter {
         holder.getTvProductName().setText(vo.getPro_name());
         holder.getTvProCount().setText(vo.getPro_count());
 
+        holder.getBtnProDetail().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //대화상자: AlertDialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
+                builder.setTitle("상품보기")
+                        .setMessage("상품개수:")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
         return view;
 
